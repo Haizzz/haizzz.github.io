02086e0aa94a3988ff8a3cc2428185d25fa8719e
Anh Le <anh@canva.com>
2023-05-12 16:09:01 +1000

    add recent work

--- src/components/recentWork/recentWork.tsx
+++ src/components/recentWork/recentWork.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 export const RecentWork = () => {
   return (
@@ -6,12 +6,16 @@ export const RecentWork = () => {
       <h3>Recent works</h3>
       <ul>
         <li>
-          <a href="https://canvatechblog.com/we-put-half-a-million-files-in-one-git-repository-heres-what-we-learned-ec734a764181">
-            We Put Half a Million files in One git Repository, Here’s What We
-            Learned
+          <a href='https://tidbits.imanhle.com'>
+            Tidbits, filter news articles with large language models
+          </a>
+        </li>
+        <li>
+          <a href='https://canvatechblog.com/we-put-half-a-million-files-in-one-git-repository-heres-what-we-learned-ec734a764181'>
+            We Put Half a Million files in One git Repository, Here’s What We Learned
           </a>
         </li>
       </ul>
     </>
-  );
-};
+  )
+}
