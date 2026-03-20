5142420aff806466dd67549468be5e688a4a0d42
Anh Le <anh@canva.com>
2026-03-20 13:17:26 +1100

    set user agent

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,16 @@ module.exports = {
       options: {
         url: `https://anhvietle.substack.com/feed`,
         name: `Substack`,
+        parserOption: {
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+            Accept:
+              "application/rss+xml,application/xml,text/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.9",
+            Referer: "https://anhvietle.substack.com/",
+          },
+        },
       },
     },
     `gatsby-plugin-react-helmet`,
